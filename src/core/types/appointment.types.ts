dc826965323bff5318c65c4df770eb1f795607a6
Franz Victorio <evenhold@gmail.com>
2026-07-02 01:59:36 -0500

    feat: implement core domain types and validation schemas using Zod
    - Define CreateAppointmentSchema with strict rules for insuredId (5 digits) and countryISO (PE/CL).
    - Derive type-safe CreateAppointmentInput and AppointmentStatus models using Zod inference.
    - Establish immutable AppointmentEntity interface with readonly properties for DynamoDB state.

---
+++ src/core/types/appointment.types.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+/**
+ * Schema de validación para campos enviados.
+ */
+export const CreateAppointmentSchema = z.object({
+  insureId: z
+    .string()
+    .length(5, { message: "El insuredId debe tener exactamente 5 caracteres." })
+    .regex(/^\d+$/, { message: "El insureId debe contener solo números." }),
+
+  scheduledId: z
+    .number({ required_error: "El scheduledId es obligatorio." })
+    .int({ message: "El scheduledId debe ser un número entero" })
+    .positive({ message: "El scheduledId debe ser un número positivo" }),
+
+  countryISO: z.enum(["PE", "CL"], {
+    errorMap: () => ({ message: "El countryISO solo puede ser PE (Perú) o CL (Chile)." }),
+  }),
+});
+
+export type CreateAppointmentInput = z.infer<typeof CreateAppointmentSchema>;
+
+/**
+ * Schema de validación para ESTADOS: pending - completed
+ */
+
+export const AppointmentStatusSchema = z.enum(["pending", "completed"]);
+
+export type AppointmentStatus = z.infer<typeof AppointmentStatusSchema>;
+
+/**
+ * Modelo de la entidad que se guardará en DynamoDB
+ */
+
+export interface AppointmentEntity extends CreateAppointmentInput {
+  readonly appointmentId: string;
+  readonly status: AppointmentStatus;
+  readonly createdAt: string;
+}
